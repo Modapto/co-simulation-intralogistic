50c771617bd4066b019aef0eb0e8afedbbdce5f1
Damiano Falcioni <damianofalcioni@users.noreply.github.com>
2025-10-29 18:18:15 +0100

    fix

---
+++ model-simulation-and-verification/src/main/java/org/adoxx/rest/AddAcceptHeaderFilter.java
@@ -0,0 +1,19 @@
+package org.adoxx.rest;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+public class AddAcceptHeaderFilter implements ContainerRequestFilter {
+
+    private static final String DEFAULT_ACCEPT = "application/json";
+
+    @Override
+    public void filter(ContainerRequestContext requestContext) {
+        // Add Accept if missing
+        if (requestContext.getHeaderString("Accept") == null) {
+            requestContext.getHeaders().putSingle("Accept", DEFAULT_ACCEPT);
+        }
+    }
+}
